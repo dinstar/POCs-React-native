6358502fd53e8b3ea97c8a6234cedcae1aae8bf6
Denis Starikov <denstar.connect@gmail.com>
2024-09-26 18:42:54 +0300

    Initial commit
    Generated by react-native@0.75.3
    
    Hello world
    
    Hello world app
    
    Initial commit
    
    Generated by react-native@0.75.3
    
    Hello world
    
    Hello world app

---
+++ App.tsx
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { Text } from 'react-native';
+
+const App = _ =>
+  <Text>Hello world!!!</Text>;
+
+export default App;
