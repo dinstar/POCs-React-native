f35744f74a2e3766819ff782f8361610e8598156
Denis Starikov <denstar.connect@gmail.com>
2024-09-27 02:25:15 +0300

    Two screens

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { Button, LogBox, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { Text } from 'react-native';
+LogBox.ignoreLogs(["Warning: ..."]);
+LogBox.ignoreAllLogs();
 
-const App = _ =>
-  <Text>Hello world!!!</Text>;
+const
+  Stack = createNativeStackNavigator();
+
+const
+  HomeScreen = ({ navigation }) =>
+    <View style={{padding:50}}>
+      <Button title="Go to Welcome" onPress={() => navigation.navigate("Welcome")} />
+    </View>,
+
+  WelcomeScreen = ({ navigation }) =>
+    <View style={{padding:50}}>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>,
+
+  App = () =>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+  </NavigationContainer>;
 
 export default App;
