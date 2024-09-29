a85d5e907b25ef77d236c9351f7bf46636c8918f
Denis Starikov <denstar.connect@gmail.com>
2024-09-30 00:23:30 +0300

    Two screen stacks

--- App.tsx
+++ App.tsx
@@ -1,31 +1,69 @@
-import React from "react";
-import { Button, LogBox, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-LogBox.ignoreLogs(["Warning: ..."]);
-LogBox.ignoreAllLogs();
+import React, { createContext, useState, useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 
 const
+  StackSelectionCtx = createContext<any>(null),
+  getStackSelectionCtx = () => useContext(StackSelectionCtx),
   Stack = createNativeStackNavigator();
 
 const
-  HomeScreen = ({ navigation }) =>
-    <View style={{padding:50}}>
-      <Button title="Go to Welcome" onPress={() => navigation.navigate("Welcome")} />
-    </View>,
+  StackSwitcher = () =>
+  {
+    const
+      { value, setValue } = getStackSelectionCtx();
+    const
+      onPress = () => setValue(isStackA => !isStackA);
+    return <View style={{ padding: 20 }}>
+      <Text style={[styles.text, { color: 'black' }]}>{`isStackA: ${value}`}</Text>
+      <TouchableHighlight onPress={onPress} style={styles.button}>
+        <Text style={styles.text}>{`Switch to Stack ${value ? 'B' : 'A'}`}</Text>
+      </TouchableHighlight>
+    </View>
+  },
+  ScreenA = () => <View><StackSwitcher/></View>,
+  ScreenB = () => <View><StackSwitcher/></View>;
 
-  WelcomeScreen = ({ navigation }) =>
-    <View style={{padding:50}}>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-    </View>,
+const ScreenStack = ({ value }) => {
+  if (value)
+    return <Stack.Navigator>
+      <Stack.Screen name="ScreenA1" component={ScreenA} />
+    </Stack.Navigator>;
+  else
+    return <Stack.Navigator>
+      <Stack.Screen name="ScreenB1" component={ScreenB} />
+    </Stack.Navigator>;
+};
 
-  App = () =>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      </Stack.Navigator>
-  </NavigationContainer>;
+const App = () => {
+  const
+    [value, setValue] = useState(true);
+  return (
+    <StackSelectionCtx.Provider value={{ value, setValue }}>
+      <NavigationContainer>
+        <ScreenStack value={value} />
+      </NavigationContainer>
+    </StackSelectionCtx.Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "gray",
+    borderRadius: 20,
+    margin: 14,
+    width: "78%",
+    height: 150,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  text: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    letterSpacing: 0.25
+  },
+});
 
 export default App;
